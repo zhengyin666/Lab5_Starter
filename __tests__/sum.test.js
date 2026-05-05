22c7ec727850315d6bb5b9f4279a5684f029cfbc
Zhengyin Yang <yzy2003666@gmail.com>
2026-05-05 00:05:34 -0700

    [Fixed]

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,9 +2,9 @@
 import { sum } from '../code-to-unit-test/sum';
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(4);
+  expect(1 + 2).toBe(3);
 });
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1,2)).toBe(4);
+    expect(sum(1,2)).toBe(3);
 });
